a64aa2f609194eb152dc7de0deb1d887581b0330
Simranjeet356 <simranjeetkaur.lnu8@rdpolytech.ca>
2025-04-08 18:40:20 -0600

    Update

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const userRoutes = require('./routes/users.js');
+require('dotenv').config();
+const PORT = process.env.PORT;
+const rateLimit = require('express-rate-limit');
+const fixedWindowRateLimit = rateLimit({
+ windowsMs: 1 * 15 * 1000,
+ max: 10,
+ message: 'Too many requests.  Please try again later.',
+});
+
+//Setup the view engine
+app.set('view engine','ejs');
+app.set('views', './views');
+
+//Middleware to serve static files
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static('public'));
+app.use(fixedWindowRateLimit);
+app.use(userRoutes);
+
+
+
+app.listen(PORT, '0.0.0.0', ()=>{
+    console.log(`Connected on port: ${PORT}`);
+});
---
+++ routes/users.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const router = express.Router();
+const userController = require('../controllers/userController.js');
+const {readData}= require ('../utils/file.js');
+
+
+router.use(express.json())
+router.use(express.urlencoded({extended: true}));
+
+
+//Router handler for our home page
+router.get('/home',  (req, res)=>{
+    res.render('home');
+});
+//api endpoint exposing the users resource
+router.get('/api/v1/users',async (req, res) =>{
+    try {
+        const data = await readData();
+        res.json(data);
+        
+    } catch (error) {
+        res.status(500).json ('Internal Server Error');
+        
+    }
+});
+
+//users post route to add a new user
+router.post('/users', userController.createUser);
+
+
+module.exports = router;
