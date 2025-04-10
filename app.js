4b49f229d83c2eef7b380f6b6067b0c579c0c1d6
Simranjeet356 <simranjeetkaur.lnu8@rdpolytech.ca>
2025-04-09 18:03:06 -0600

    Update

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ const userRoutes = require('./routes/users.js');
 require('dotenv').config();
 const PORT = process.env.PORT;
 const rateLimit = require('express-rate-limit');
+const mongoose = require('mongoose');
+const uri = 'mongodb+srv://simranjeetkaurlnu8:Bathinda1994@cluster0.cilbj10.mongodb.net/';
+const YAML = require('yamljs');
+const swaggerUI = require('swagger-ui-express');
+const swaggerDocument = YAML.load('./swagger.yaml');
+
 const fixedWindowRateLimit = rateLimit({
  windowsMs: 1 * 15 * 1000,
  max: 10,
@@ -15,14 +21,19 @@ app.set('view engine','ejs');
 app.set('views', './views');
 
 //Middleware to serve static files
+app.use('/api/docs', swaggerUI.serve,swaggerUI.setup(swaggerDocument));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'));
 app.use(fixedWindowRateLimit);
 app.use(userRoutes);
 
+mongoose.connect(uri).then(
+    async () =>{
+        console.log('connected to MongoDB Server');
+        app.listen(PORT, '0.0.0.0', ()=>{
+            console.log(`Connected on port: ${PORT}`);
+    });
+    }
 
-
-app.listen(PORT, '0.0.0.0', ()=>{
-    console.log(`Connected on port: ${PORT}`);
-});
+).catch((err) => { console.log(`Error: ${err}`)});
